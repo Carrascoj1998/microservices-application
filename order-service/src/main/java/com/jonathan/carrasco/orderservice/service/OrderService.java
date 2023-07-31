d1b03f1e17e08b79b5045073321175b74d531cfe
Carrascoj1998 <carrascoj1998@gmail.com>
2023-07-31 16:34:17 -0500

    Secured microservices using Keycloak

--- order-service/src/main/java/com/jonathan/carrasco/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/jonathan/carrasco/orderservice/service/OrderService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -27,12 +28,13 @@ public class OrderService {
        Order order = new Order();
        order.setOrderNumber(UUID.randomUUID().toString());
 
-        List<OrderLineItems> orderLineItems =
-                orderRequest.getOrderLineItemsDtoList()
-                .stream()
-                .map(this::mapToDto)
-                .toList();
-
+        List<OrderLineItems> orderLineItems = new ArrayList<>();
+        List<OrderLineItemsDto> dtoList = orderRequest.getOrderLineItemsDtoList();
+        if (dtoList != null) {
+            orderLineItems = dtoList.stream()
+                    .map(this::mapToDto)
+                    .toList();
+        }
         order.setOrderLineItemsList(orderLineItems);
 
         List<String> skuCodes = order.getOrderLineItemsList().stream()
